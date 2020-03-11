afe00ad01dc53f90432059e8a8be870654ab06ad
cybertekschool <github@cybertekschool.com>
2020-03-11 15:49:24 -0400

    03/11/2020 Office Hour

--- src/OfficeHours/Practice_03_11_2020/OfficeHourNote.java
+++ src/OfficeHours/Practice_03_11_2020/OfficeHourNote.java
@@ -2,6 +2,91 @@ package OfficeHours.Practice_03_11_2020;
 
 public class OfficeHourNote {
     /*
+    03/11/2020
+Practice Tasks: print statements (print & println)
+				escape sequences
+				comments
+				Variables
+
+
+WarmUp task:
+	1. right click src folder in your project -> new -> package
+		and create package "OfficeHours"
+
+	2. right click package "OfficeHours" -> select -> package
+		and create package "Practice_11_03_2020"
+
+			NOTE: "Practice_11_03_2020" is a sub PACKAGE
+
+
+main method:to run the codes as java application
+
+		public static void main(String[] args){
+
+		}
+
+
+print statements:
+		System.out.println(); : prints the output
+							after printing the output, breaks the line
+
+		System.out.print(); : prints the output
+							after printing the output, DOES NOT break the line
+
+
+"" : used for text values
+		"wordds go here"
+
+
+{ } : used for giving the body for the blocks of code
+
+
+escape sequences: MUST be given in a ""
+		\t: paragraph space
+		\n: new line
+		\": double quote
+		\\: single back slash \
+
+
+single line comment: starts with //
+				// comment goes here
+
+Multi line comments: starts with /* ends with *\
+
+     */
+
+/*
+    declear variable:
+    DataType variableName = Data;
+
+    String: for texts
+    String is not a primitive
+
+    primitive data types:
+    byte, short, int, long: only accepts whole numbers
+    float , double: accpect decimals
+
+
++:
+    when we apply + to  of text, it concates it
+
+    when we apply + to numbers ==> addition
+
+    text + text ==> concatenation
+
+    text + number ==> concatenation
+
+    number + text ==> concatenation
+
+    number + number ==> addition
+
+
+
+
+
+
+
+
 
 
      */
