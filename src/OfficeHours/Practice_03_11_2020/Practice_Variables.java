e68d4cdda2fc0fb755c125f62bb105ed2944939c
cybertekschool <github@cybertekschool.com>
2020-03-11 15:47:33 -0400

    03/11/2020 Office Hour

--- src/OfficeHours/Practice_03_11_2020/Practice_Variables.java
+++ src/OfficeHours/Practice_03_11_2020/Practice_Variables.java
@@ -12,19 +12,32 @@ public class Practice_Variables {
 
     public static void main(String[] args) {
 
-        String brand = "BMW";
+        String brand = "Toyota";
         short year = 2020;
-        float price = 50000.50f;
-        String model = "X6";
-        short maximumMiles = 32767;
+        float price = 30000.25f;
+        String model = "Camry";
+        long maximumMiles = 1000000;
 
         System.out.println( brand );
         System.out.println( "brand" );
                 // variables that are decleared ina block, must be initialized before use
 
+     /*
+     output:
+        The vehicle 2020 BMW X6, has 32767 miles, price is 50000.5 USD
+      */
+
+    System.out.println("The vehicle " + year + " "+brand+" "+model+", has "+maximumMiles
+                + " miles, price is " +price + " USD" );
+
+
+
+      /*
+      addition vs concatenation:
         System.out.println( 9 + 10); // 19, addition: both left and right side of the + vaules are numbers
         System.out.println( "9" + "10"); //910
         System.out.println( "9" + 10 ); //910
+    */
 
 
     }
