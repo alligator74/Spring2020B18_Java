17056268228ab813e8eb8e5fc8f3d41293ad856a
cybertekschool <github@cybertekschool.com>
2020-03-17 15:04:57 -0400

    Day06 class

---
+++ src/day06_Shorthnd_LogicalOperators/IfStatementsPractice.java
@@ -0,0 +1,56 @@
+package day06_Shorthnd_LogicalOperators;
+
+public class IfStatementsPractice {
+
+    public static void main(String[] args) {
+        int a =100;
+        int b = 200;
+
+        if(a > b){ // false
+            System.out.println(a+" is greater than "+b);
+        }
+
+        boolean bGreater = b > a;
+        if(bGreater){
+            System.out.println(b+" is greater than " + a );
+        }
+
+
+        if(a ==  b){
+            System.out.println(a +" is equal to " + b );
+        }
+
+        System.out.println("==================================================");
+        int x = 300;
+        int y = 300;
+        boolean xGreater = x > y;
+        boolean yGreater = y > x;
+
+        if(xGreater){
+            System.out.println(x+" is gereater than "+y);
+        }
+
+        if(yGreater){
+            System.out.println(y+" is greater than "+x);
+        }
+
+/*
+        if(xGreater == false && yGreater == false){
+            System.out.println(x+" is equal to "+y);
+        }
+*/
+
+/*
+        if(!xGreater  && !yGreater){
+            System.out.println(x+" is equal to "+y);
+        }
+*/
+
+        if(xGreater == yGreater){
+            System.out.println(x+" is equal to "+y);
+        }
+
+
+    }
+
+}
