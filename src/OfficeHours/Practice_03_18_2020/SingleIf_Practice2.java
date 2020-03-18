0fb3f354d12ad3e0023ca13b946b973241e018d3
cybertekschool <github@cybertekschool.com>
2020-03-18 16:27:02 -0400

    Office Hour: 03/18/2020

---
+++ src/OfficeHours/Practice_03_18_2020/SingleIf_Practice2.java
@@ -0,0 +1,32 @@
+package OfficeHours.Practice_03_18_2020;
+
+public class SingleIf_Practice2 {
+
+    public static void main(String[] args) {
+        byte age = 20 ;
+        boolean eligibleToBuy =  age > 21 || age ==21;  // age >=21
+
+        if( eligibleToBuy ){
+            System.out.println("Here is your Vodka");
+        }
+
+        if( ! eligibleToBuy ){
+            System.out.println("Go drink your milk");
+        }
+
+
+        boolean feeling = false;
+
+        if(feeling == false){
+            System.out.println("don't be sad");
+        }
+
+        if(feeling == true){
+            System.out.println("Stay happy");
+        }
+
+
+
+    }
+
+}
