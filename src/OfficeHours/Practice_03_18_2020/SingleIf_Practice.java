d78bab0dbb87f14fee12ce3ba4570107f87cfe66
cybertekschool <github@cybertekschool.com>
2020-03-18 16:13:49 -0400

    Office Hour: 03/18/2020

---
+++ src/OfficeHours/Practice_03_18_2020/SingleIf_Practice.java
@@ -0,0 +1,39 @@
+package OfficeHours.Practice_03_18_2020;
+
+public class SingleIf_Practice {
+
+
+    public static void main(String[] args) {
+        int a =200;
+        int b = 200;
+
+        boolean aGreater = a > b;
+        boolean bGreater = b > a;
+
+        boolean aEqualB =  a == b;
+
+  //     boolean aEqualB = aGreater == false && bGreater ==false;
+                            // if a is not greater than  and b is not greater than a, that means a ==b
+
+   //      boolean aEqualB = aGreater == bGreater;
+                 // if a is not greater than  and b is not greater than a, that means a ==b
+
+
+    if(aGreater) {
+        System.out.println(a + " is greater number");
+    }
+
+    if(bGreater) {
+        System.out.println(b + " is greater number");
+    }
+
+
+    if(aEqualB) {
+        System.out.println(a + " is equal to " + b);
+    }
+
+
+
+    }
+
+}
