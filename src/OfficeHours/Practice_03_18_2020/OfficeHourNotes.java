2aaa9b162b8b2af331f5e9451bcc70d3e51fa141
cybertekschool <github@cybertekschool.com>
2020-03-18 16:28:11 -0400

    Office Hour: 03/18/2020

---
+++ src/OfficeHours/Practice_03_18_2020/OfficeHourNotes.java
@@ -0,0 +1,74 @@
+package OfficeHours.Practice_03_18_2020;
+
+public class OfficeHourNotes {
+    /*
+
+    03/18/2020
+Practice Topics:
+			All the operators
+			single if statement
+
+Package name: Practice_03_18_2020
+
+all operators we have learned:
+								+, -, *, /, %
+
+								+=, -=, *=, /=, %=
+
+								+, -, --, ++
+
+								>, >=, <, <=, ==, !=,   !, &&, ||
+
+
+%: numerator - (denominator * Whole Number for result)
+
+	10 / 3 = 3.333333;
+	remiander: 10 - (3 * 3)
+				10 - 9
+				1
+
+
+post increment/decrement: first passing the current value, and then increase/decrease by 1
+					place the operators after the vairble name
+
+					int a = 20;
+						a++;
+
+
+pre incraement/decrement:  increase/decrease value by 1 immediately
+					place the operator before the variable name
+
+					int a = 20;
+					++a;
+
+
+
+
+
+	warmup task:
+		write a program that can check the age of a person if he/she is qualified to buy alchohol
+
+		write a code of happiness
+
+
+|| : as long as one condition is true ==> true
+
+&& : as long as one condition is false ==> false
+
+
+
+single if statement:
+
+		if(Condition){
+			statements;
+
+		}
+
+
+
+     */
+
+
+
+
+}
