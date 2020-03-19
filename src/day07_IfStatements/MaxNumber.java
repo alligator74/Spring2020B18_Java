c0236242acbdb1433f6b5e4ca2dc8c7342ec5293
cybertekschool <github@cybertekschool.com>
2020-03-19 10:53:37 -0400

    Day07 class

---
+++ src/day07_IfStatements/MaxNumber.java
@@ -0,0 +1,38 @@
+package day07_IfStatements;
+/*
+2. write a java program that accepts three numbers and return the maximum number
+				(assume that none of them are equal)
+ */
+
+public class MaxNumber {
+
+    public static void main(String[] args) {
+        double a = 100.5;
+        double b = 200.5;
+        double c = 300.5;
+
+        boolean aGreater = a > b && a > c;  // if a is greater than both b and c, then a is maximum
+
+        boolean bGreater = b > a && b > c; // if b is greater than both a and c, then b is maximum
+
+    //    boolean bGreater2 = b > a || b > c; // if b is greater than either a and c, then b is maximum
+
+        boolean cGreater = c > a && c > b;  //if c is greater than both a nd b, then c is maximum
+
+        if(aGreater) {
+            System.out.println(a + " is greater number");
+        }
+
+        if(bGreater) {
+            System.out.println(b + " is greater number");
+        }
+
+        if(cGreater) {
+            System.out.println(c + " is greater number");
+        }
+
+
+
+    }
+
+}
